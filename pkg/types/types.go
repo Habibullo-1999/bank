5ff43930815cd976db853e6f28ab1e726ffcde55
Habibullo <63663696+Habibullo-1999@users.noreply.github.com>
2021-04-02 00:52:36 +0500

    Добавлени статус к струтуре Payment

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,8 +6,20 @@ type Money int64
 // Category представляет собой категорию, в которой был совершён платёж (авто, аптеки, рестораны и т.п.).
 type Category string
 
+
+//Status ...
+type Status string
+
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
+
 type Payment struct {
 	ID int 
 	Amount Money
 	Category Category
+	Status Status
 }
